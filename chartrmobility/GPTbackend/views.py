5724dc0da9d3ae85097b323ae3663042231405d3
mishraanis <mishraanis@gmail.com>
2023-04-01 03:08:29 +0530

    requirements.txt added

--- chartrmobility/GPTbackend/views.py
+++ chartrmobility/GPTbackend/views.py
@@ -9,15 +9,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("OPENAI_KEY", None)
-# Create your views here.
 class GPTgenerator(APIView):
     def post(self, request, format=None):
         print(request.data['input_text'], " input text")
-        # use OpenAI API to generate reponse to user input text
         openai.api_key = api_key
         print(openai.api_key, " openai api key")
         input_text = request.data['input_text']
-        # Generate a response using the OpenAI API
         # response = openai.Completion.create(
         #     engine="gpt-3.5-turbo",
         #     prompt=input_text,
